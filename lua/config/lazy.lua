62e4d1271ffaa74206e8d96987465fb8355f1ed1
roninlp <ronin.lp@gmail.com>
2023-11-17 16:02:54 +0330

    telescope config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,8 +13,13 @@ require("lazy").setup({
       colorscheme = "solarized-osaka",
     } },
     -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
