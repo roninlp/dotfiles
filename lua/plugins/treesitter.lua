406a66b47abc7ed703ad9338bce2843c54ece76c
roninlp <ronin.lp@gmail.com>
2023-11-17 17:32:59 +0330

    lsp and treesitter setup

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "nvim-treesitter/vim-treesitter",
+    opts = {
+      ensure_installed = {
+        "astro",
+        "cmake",
+        "css",
+        "gitignore",
+        "go",
+        "graphql",
+        "http",
+        "scss",
+        "sql",
+        "svelte",
+        "yaml",
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+
+      -- MDX
+      vim.filetype.add({
+        extension = {
+          mdx = "mdx",
+        },
+      })
+      vim.treesitter.language.register("markdown", "mdx")
+    end,
+  },
+}
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,83 @@
+return {
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      table.insert(opts.routes, {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+        opts = { skip = true },
+      })
+
+      opts.presets.lsp_doc_border = true
+    end,
+  },
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 10000,
+    },
+  },
+
+  -- statusline
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = {
+      options = {
+        theme = "catppuccin",
+      },
+    },
+  },
+
+  -- filename
+  {
+    "b0o/incline.nvim",
+    dependencies = { "catppuccin/nvim" },
+    event = "BufReadPre",
+    priority = 1200,
+    config = function()
+      local colors = require("catppuccin.palettes").get_palette("mocha")
+      require("incline").setup({
+        highlight = {
+          groups = {
+            InclineNormal = { guifg = colors.green, guibg = colors.crust },
+            InclineNormalNC = { guibg = colors.base, guifg = colors.text },
+          },
+        },
+        window = { margin = { vertical = 0, horizontal = 1 } },
+        hide = {
+          cursorline = true,
+        },
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+          if vim.bo[props.buf].modified then
+            filename = "[+]" .. filename
+          end
+
+          local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+          return { { icon, guifg = color }, { " " }, { filename } }
+        end,
+      })
+    end,
+  },
+
+  -- bufferline
+  {
+    "akinsho/bufferline.nvim",
+    keys = {
+      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next Tab" },
+      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev Tab" },
+    },
+    opts = {
+      options = {
+        mode = "tabs",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      },
+    },
+  },
+
+  -- statusline
+}
